298e1ff91bec037d88c55fea89dc5ac61798291a
R.Minelli88 <R.Minelli88@142c74db-416a-1121-bd68-d796f0770063>
2011-11-24 12:29:34 +0000

    Adding first DB schema

---
+++ doc/genesis_db.sql
@@ -0,0 +1,197 @@
+CREATE DATABASE Genesis_db;
+
+CREATE TABLE Developers
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE Revisions
+(
+id int NOT NULL AUTO_INCREMENT,
+comment varchar(255) NOT NULL,
+revision_number int NOT NULL,
+developer_id int NOT NULL,
+revision_date datetime,
+PRIMARY KEY (id),
+FOREIGN KEY (developer_id) REFERENCES Developers(id)
+);
+
+CREATE TABLE Projects
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+source_language varchar(255) NOT NULL,
+source_dialect varchar(255),
+revision_id int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (revision_id) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Packages
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+owner int NOT NULL,
+belongs_to_project int NOT NULL,
+belongs_to_package int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (owner) REFERENCES Developers(id),
+FOREIGN KEY (belongs_to_project) REFERENCES Project(id),
+FOREIGN KEY (belongs_to_package) REFERENCES Packages(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Packages
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+owner int NOT NULL,
+belongs_to_project int NOT NULL,
+belongs_to_package int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (owner) REFERENCES Developers(id),
+FOREIGN KEY (belongs_to_project) REFERENCES Projects(id),
+FOREIGN KEY (belongs_to_package) REFERENCES Packages(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Classes
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+owner int NOT NULL,
+belongs_to_package int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (owner) REFERENCES Developers(id),
+FOREIGN KEY (belongs_to_package) REFERENCES Packages(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Methods
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+signature varchar(255) NOT NULL,
+access_control_qualifier varchar(255) NOT NULL,
+declared_return_type varchar(255) NOT NULL,
+belongs_to_class int NOT NULL,
+owner int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (owner) REFERENCES Developers(id),
+FOREIGN KEY (belongs_to_class) REFERENCES Classes(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Attributes
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+signature varchar(255) NOT NULL,
+access_control_qualifier varchar(255) NOT NULL,
+declared_type varchar(255) NOT NULL,
+belongs_to_class int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (belongs_to_class) REFERENCES Classes(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE InheritaceDefinitions
+(
+subclass int NOT NULL,
+superclass int NOT NULL,
+access_control_qualifier varchar(255) NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (subclass, superclass, revision),
+FOREIGN KEY (subclass) REFERENCES Classes(id),
+FOREIGN KEY (superclass) REFERENCES Classes(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Candidates
+(
+id int NOT NULL,
+candidate int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (id, candidate, revision),
+FOREIGN KEY (candidate) REFERENCES Methods(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE Invocations
+(
+invoked_by int NOT NULL,
+candidates int NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (invoked_by, candidates, revision),
+FOREIGN KEY (invoked_by) REFERENCES Methods(id),
+FOREIGN KEY (candidates) REFERENCES Candidates(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE BugTrackerHistory
+(
+id int NOT NULL,
+previous_assignee int NOT NULL,
+PRIMARY KEY (id, previous_assignee),
+FOREIGN KEY (previous_assignee) REFERENCES Developers(id)
+);
+
+CREATE TABLE BugTrackerInfo
+(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+descriptions varchar(255) NOT NULL,
+belongs_to_project int NOT NULL,
+status varchar(255) NOT NULL,
+assignee int NOT NULL,
+bug_history int NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (belongs_to_project) REFERENCES Projects(id),
+FOREIGN KEY (bug_history) REFERENCES BugTrackerHistory(id),
+FOREIGN KEY (assignee) REFERENCES Developers(id)
+);
+
+CREATE TABLE MethodMetrics
+(
+method_id int NOT NULL,
+name varchar(255) NOT NULL,
+value varchar(255) NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (method_id, name, value, revision),
+FOREIGN KEY (method_id) REFERENCES Methods(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+CREATE TABLE ClassMetrics
+(
+class_id int NOT NULL,
+name varchar(255) NOT NULL,
+value varchar(255) NOT NULL,
+revision int NOT NULL,
+PRIMARY KEY (class_id, name, value, revision),
+FOREIGN KEY (class_id) REFERENCES Classes(id),
+FOREIGN KEY (revision) REFERENCES Revisions(id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
