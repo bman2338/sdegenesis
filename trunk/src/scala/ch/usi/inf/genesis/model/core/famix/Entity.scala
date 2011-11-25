be17a0b34fcba151f4119c43279bcb47bd670114
remo.lemma@gmail.com <remo.lemma@gmail.com@142c74db-416a-1121-bd68-d796f0770063>
2011-11-25 09:49:14 +0000

    Committing FAMIX keywords

--- trunk/src/scala/ch/usi/inf/genesis/model/core/famix/Entity.scala
+++ trunk/src/scala/ch/usi/inf/genesis/model/core/famix/Entity.scala
@@ -1,5 +1,5 @@
 package ch.usi.inf.genesis.model.core.famix
 
-class Entity (val name: String)  extends FamixObject {
+class Entity (val name:String) extends FamixObject {
   protected var uniqueName:String = ""
 }
--- trunk/src/scala/ch/usi/inf/genesis/model/core/famix/InvocationRelation.scala
+++ trunk/src/scala/ch/usi/inf/genesis/model/core/famix/InvocationRelation.scala
@@ -2,5 +2,6 @@ package ch.usi.inf.genesis.model.core.famix
 
 import scala.collection.mutable.HashMap
 
-class InvocationRelation(val invokedBy: MethodEntity,  
-						 val candidates: HashMap[String, MethodEntity] = new HashMap()) extends OneToManyRelation
+class InvocationRelation(val source: Entity) extends OneToManyRelation {
+  val candidates: HashMap[String, MethodEntity] = new HashMap()
+}
