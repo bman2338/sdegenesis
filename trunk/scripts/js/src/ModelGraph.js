f102baf7e95d0156b43c27a995c1b3009cc8722d
patrick.zulian <patrick.zulian@142c74db-416a-1121-bd68-d796f0770063>
2011-11-28 19:46:59 +0000

    added another example chart

--- trunk/scripts/js/src/ModelGraph.js
+++ trunk/scripts/js/src/ModelGraph.js
@@ -1,30 +1,35 @@
-function tree() {
-d3.select("#chart").html("");
-    var json = //{
-      //  "name": "",
-      //  "children": [
-            {
-                "name": "Object",
-                "children": [
-                    {
-                        "name": "ModelObject",
-                        "children": [
-                            {"name": "FamixObject", "size": 3938},
-                            {"name": "ValueObject", "size": 3812},
-                    
-                            ]
-                    },
-                    {
-                        "name": "Navigator",
-                        "children": [
-                            {"name": "BreadthFirstNavigator", "size": 3534},
-                            {"name": "DepthFirstNavigator", "size": 5731}
-                            ]
-                    }
-                    ]
-           // }
-           // ]
+function data() {
+    
+        var json=   {
+            "name": "Object",
+            "children": [
+                {
+                    "name": "ModelObject",
+                    "children": [
+                        {"name": "FamixObject", "size": 3938},
+                        {"name": "ValueObject", "size": 3812},
+                        
+                        ]
+                },
+                {
+                    "name": "Navigator",
+                    "children": [
+                        {"name": "BreadthFirstNavigator", "size": 3534},
+                        {"name": "DepthFirstNavigator", "size": 5731}
+                        ]
+                }
+                ]
     };
+        
+        return json;
+        
+}
+
+
+
+function tree() {
+    d3.select("#chart").html("");
+    var json = data();
     
     var r = 960/2;
 	var tree = d3.layout.tree()
@@ -33,42 +38,107 @@ d3.select("#chart").html("");
     
     var diagonal = d3.svg.diagonal.radial()
         .projection(function(d) { return [ d.y, d.x /180 * Math.PI]; });
-        
-        
-        
+    
+    
+    
     var vis = d3.select("#chart")
         .append("svg:svg")
         .attr("width", r*3)
         .attr("height", r*3 - 150)
         .append("svg:g")
         .attr("transform", "translate(" + (r + 50)+ "," + (r + 50) + ")");
-        
+    
     var nodes = tree.nodes(json);
     var link = vis.selectAll("path.link")
-                .data(tree.links(nodes))
-                .enter().append("svg:path")
-                .attr("class", "link")
-                .attr("d", diagonal);
-                
-    var node = vis.selectAll("g.node")
-                .data(nodes)
-                .enter().append("svg:g")
-                .attr("class", "node")
-                .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")" })
-               // .attr("transform", function(d) { return "scale(" + 1 + ")translate(" + 1 + ")" });
+        .data(tree.links(nodes))
+        .enter().append("svg:path")
+        .attr("class", "link")
+        .attr("d", diagonal);
     
-    node.append("svg:circle")
-        .attr("r", 10);
+    var node = vis.selectAll("g.node")
+        .data(nodes)
+        .enter().append("svg:g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")" })
+        // .attr("transform", function(d) { return "scale(" + 1 + ")translate(" + 1 + ")" });
         
+        node.append("svg:circle")
+        .attr("r", 10);
+    
     
     node.append("svg:text")
-    .attr("dx", function(d) { return d.x < 180 ? 40 : -40; })
-    .attr("dy", ".31em")
-    .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-    .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
-    .text(function(d) { return d.name; });
+        .attr("dx", function(d) { return d.x < 180 ? 40 : -40; })
+        .attr("dy", ".31em")
+        .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+        .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
+        .text(function(d) { return d.name; });
 }
 
+
+
 function graph() {
+    //TODO
+    d3.select("#chart").html("");
+}
+
+
+
+
+function hist() {
 d3.select("#chart").html("");
+    var n = 10000, // number of trials
+    m = 10,    // number of random variables
+    data = [];
+    
+    // Generate an Irwin-Hall distribution.
+    for (var i = 0; i < n; i++) {
+        for (var s = 0, j = 0; j < m; j++) {
+            s += Math.random();
+        }
+        data.push(s);
+    }
+    
+    var w = 400,
+        h = 400;
+    
+    var histogram = d3.layout.histogram()
+        (data);
+    
+    var x = d3.scale.ordinal()
+        .domain(histogram.map(function(d) { return d.x; }))
+        .rangeRoundBands([0, w]);
+    
+    var y = d3.scale.linear()
+        .domain([0, d3.max(histogram, function(d) { return d.y; })])
+        .range([0, h]);
+    
+    var vis = d3.select("#chart").append("svg:svg")
+        .attr("width", w)
+        .attr("height", h)
+        .append("svg:g")
+        .attr("transform", "translate(.5)");
+    
+    vis.selectAll("rect")
+        .data(histogram)
+        .enter().append("svg:rect")
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + (h - y(d.y)) + ")"; })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.y); })
+        .attr("height", 0)
+        .transition()
+        .duration(750)
+        .attr("y", 0)
+        .attr("height", function(d) { return y(d.y); });
+    
+    vis.append("svg:line")
+        .attr("x1", 0)
+        .attr("x2", w)
+        .attr("y1", h)
+        .attr("y2", h);
+    
+}
+
+
+function stacked() {
+//TODO http://mbostock.github.com/d3/ex/stack.html
 }
