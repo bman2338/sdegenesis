18aed9ac6af6b38cfc35888b9f841208ddc1a89c
masiar88 <masiar88@142c74db-416a-1121-bd68-d796f0770063>
2011-11-29 12:36:49 +0000

    added empty ModelSaver

---
+++ trunk/src/scala/ch/usi/inf/genesis/model/navigation/ModelSaver.scala
@@ -0,0 +1,14 @@
+package ch.usi.inf.genesis.model.navigation
+
+import ch.usi.inf.genesis.model.core.ModelObject
+import ch.usi.inf.genesis.model.navigation.NavigatorOption._
+import ch.usi.inf.genesis.model.core.FAMIX
+
+
+class ModelSaver extends ModelVisitor {
+	
+  def visit(obj: ModelObject): NavigatorOption = {
+
+  }
+
+}
