561caf2787b5a6e210803d75b70d0d40f023118a
patrick.zulian <patrick.zulian@142c74db-416a-1121-bd68-d796f0770063>
2011-11-30 14:47:48 +0000

    Added tree navigation in vis

--- scripts/js/src/Trees.js
+++ scripts/js/src/Trees.js
@@ -22,7 +22,8 @@ el = el.append("list");
 jQuery.each(json, function() {
     var that = this;
     var callback = function(ev) { 
-        tree(that, target) 
+        d3.select("#monitor").html(that.name) 
+        hTree(that, target) 
     };
     
     el.append("li").html("a").html(that.name).on("click", 
@@ -30,18 +31,24 @@ jQuery.each(json, function() {
     );//.on("mouseover", callback);
 
 });
-tree(json[0], target);
+ d3.select("#monitor").html(json[0].name)
+hTree(json[0], target);
 
 }
 
-function tree(root, target) {
+function isBranch(d) {
+    return (d.children && d.children.length > 1) 
+}
+
+function hTree(root, target) {
     d3.select(target).html("");
     var json = root;
 //	d3.json("data3.json", 
 	
 //	function(json) {
     
-		var r = 720/2;
+    var offset = 0;
+		var r = 1024/2;
 		var tree = d3.layout.tree()
 		.size([360, r - 120])
 		.separation(function(a,b) { return (a.parent == b.parent ? 1: 2)/a.depth });
@@ -56,7 +63,7 @@ function tree(root, target) {
 		.attr("width", r*3)
 		.attr("height", r*3 - 150)
 		.append("svg:g")
-		.attr("transform", "translate(" + (r )+ "," + (r ) + ")");
+		.attr("transform", "translate(" + (r+offset )+ "," + (r+offset ) + ")");
 
 		var nodes = tree.nodes(json);
 		var link = vis.selectAll("path.link")
@@ -68,12 +75,32 @@ function tree(root, target) {
 		var node = vis.selectAll("g.node")
 		.data(nodes)
 		.enter().append("svg:g")
-		.attr("class", "node")
+		.attr("class", function(d) { var clazz = "node"; if(isBranch(d)) { return " Branch"; } return clazz;})
 		.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")" })
 		// .attr("transform", function(d) { return "scale(" + 1 + ")translate(" + 1 + ")" });
 
+
+ 
 		node.append("svg:circle")
-		.attr("r", 2.3);
+		.attr("r", function(d) 
+         {
+           if(d.children)
+                return d.children.length/5 + 4; 
+            return  3; }).on("mouseover", function(d, i) { 
+                d3.select("#monitor").html(d.name) 
+                
+            }).on("click", function(d, i) {
+                if(isBranch(d)) {
+                        if(d.hParent) {
+                            hTree(d.hParent, target);
+                            d.hParent = null;
+                        } else {
+                            d.hParent = root;
+                            hTree(d, target) 
+                        }
+                    }
+            });
+        
 
 
 		node.append("svg:text")
@@ -81,6 +108,8 @@ function tree(root, target) {
 		.attr("dy", ".31em")
 		.attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
 		.attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
-		.text(function(d) { return d.name; });
+		//.text(function(d) { return d.name; });
+        
+
 //	});
 }
