9fd2ea7ded46fd92e395a2379a9ada99d588f6cb
patrick.zulian <patrick.zulian@142c74db-416a-1121-bd68-d796f0770063>
2011-12-09 19:54:29 +0000

    Added force directed graph

---
+++ scripts/js/src/force.js
@@ -0,0 +1,55 @@
+
+function forceDirectedGraph(nodes, edges) {
+
+
+    var w = 900,
+    h = 600,
+    fill = d3.scale.category20();
+
+var vis = d3.select("#chart").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+    //.attr("z-index", 100);
+
+
+  var force = d3.layout.force()
+      .charge(-20)
+      .linkDistance(10)
+      .nodes(nodes)
+      .links(edges)
+      .size([w, h])
+      .start();
+
+  var link = vis.selectAll("line.link")
+      .data(edges)
+    .enter().append("svg:line")
+      .attr("class", "link")
+      .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+  var node = vis.selectAll("circle.node")
+      .data(nodes)
+    .enter().append("svg:circle")
+      .attr("class", "node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", 5)
+      .style("fill", function(d) { return fill(d.group); })
+      .call(force.drag);
+
+  node.append("svg:title")
+      .text(function(d) { return d.name; });
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+}
