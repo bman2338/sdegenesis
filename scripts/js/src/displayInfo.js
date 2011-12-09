3d375228159d6b7e2556adb92b4d18eaecc1eb48
metz85 <metz85@gmail.com>
2011-12-09 13:58:26 +0000

    added metrics and properties test

--- scripts/js/src/displayInfo.js
+++ scripts/js/src/displayInfo.js
@@ -1,12 +1,26 @@
 function createInfo(d){
 	var result = "";
-	
-	for(x in d){
-		result += x;
-		result += ": ";
-		result += d[x];	
-		result += " ";
-	}
+	if(d.properties){
+        result +="<div id='dispProperties'>";
+        for(x in d.properties){
+            result += x;
+            result += ": ";
+            result += d[x];	
+            result += " ";
+        }
+        result += "</div>";
+    }
+    
+    if(d.metrics){
+        result +="<div id='dispMetrics'>";
+        for(x in d.metrics){
+            result += x;
+            result += ": ";
+            result += d.metrics[x];	
+            result += " ";
+        }
+        results += "</div>";
+    }
     
 	return result;
 }
