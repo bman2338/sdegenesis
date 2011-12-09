109c3e82382c48084c9f8f395f5cc4698fdc94a8
luca.ponzanelli <luca.ponzanelli@gmail.com>
2011-12-09 00:15:07 +0000

    Fixed SVNCrawler bug while updating repository. Improved Service, parallel analysis supported. Modified RevisionInfoMutator, it now pushes list of revisions involved.

--- src/scala/ch/usi/inf/genesis/server/Service.scala
+++ src/scala/ch/usi/inf/genesis/server/Service.scala
@@ -5,7 +5,6 @@ package scala.ch.usi.inf.genesis.server
 
 
 import java.net.ServerSocket
-import collection.mutable.HashMap
 import ch.usi.inf.genesis.data.repository.FamixLanguage
 import ch.usi.inf.genesis.model.core.famix.RevisionEntity
 import java.io.{File, BufferedReader, InputStream, InputStreamReader}
@@ -18,6 +17,11 @@ import ch.usi.inf.genesis.model.navigation.{ModelPrinter, BreadthFirstNavigator}
 import java.util.logging.SimpleFormatter
 import java.text.SimpleDateFormat
 import java.util.Date
+import ch.usi.inf.genesis.model.ModelGenerator
+import ch.usi.inf.genesis.model.extractors.GraphExtractor
+import scala.ch.usi.inf.genesis.database.MongoDBWrapper
+import ch.usi.inf.genesis.model.core.{StringValue, IntValue}
+import collection.mutable.{ListBuffer, HashMap}
 
 
 object Main {
@@ -35,6 +39,9 @@ object Main {
 
 class Service(val port: Int) {
 
+  val inFamixPath = "inFamix"
+  val defaultAnalysisFolder = "analysis_folder"
+
   def run() {
     try {
       println("Running...");
@@ -43,99 +50,82 @@ class Service(val port: Int) {
       while (true) {
         val client = server.accept();
         println("Accepted connection from " + client.getInetAddress)
-        val input = client.getInputStream();
+        val input = client.getInputStream;
 
         asMap(input) match {
           case None =>
-          case Some(map) =>
+          case Some(map) => {
 
 
+            //Retrieve Repository-Parameters' Value
+            val prjName = extractParameterValue(map, RepositoryOpts.PROJECT_NAME)
+            val repository = extractParameterValue(map, RepositoryOpts.URL)
+            val repoType = extractParameterValue(map, RepositoryOpts.TYPE)
+
+            println("REPO:\t" + repository)
+            println("PROJECT:\t" + prjName)
+            println("TYPE:\t" + repoType)
+
+            //Retrieve Bugtracker-Parameters' Value
+            val btPrjName = extractParameterValue(map, BugTrackerOpts.PROJECT_NAME)
+            val btURL = extractParameterValue(map, BugTrackerOpts.URL)
+            val btType = extractParameterValue(map, BugTrackerOpts.TYPE)
+
+            println("BUG_TRACKER_PROJECT_NAME:\t" + btPrjName)
+            println("BUG_TRACKER_URL:\t" + btURL)
+            println("BUG_TRACKER_TYPE:\t" + btType)
+
+
+            //Create an Analysis Folder
+            val analysisPath = createAnalysisFolder(prjName, inFamixPath)
+
+
+
+            //TODO do not use actor if bug tracker is not needed (case NONE)
             actors.Scheduler.execute({
-              //TODO bugtracker
-              var btPrjName = ""
-              map.get("projectBugTrackerName") match{
-                  case Some(s: String) => btPrjName = s
-                  case _ =>
+            var btCrawler : BugTrackerCrawler  = null
+            btType match{
+              case BugTrackerType.BUGZILLA =>{
+                btCrawler = new BugzillaCrawler(btURL)
+                println("Bugzilla Crawler Start...")
+                val list = btCrawler.getBugList(btPrjName)
+                println("Bugzilla Crawler Done.")
+                println("Total Bugs Grabbed: " + list.count((BugEntity) => true))
               }
-
-              var bugTrackerURL = ""
-              map.get("projectBugTracker") match{
-                  case Some(s: String) => bugTrackerURL = s
-                  case _ =>
-              }
-              println("BUG_TRACKER_URL: " + bugTrackerURL)
-
-              var btCrawler : BugTrackerCrawler  = null
-              map.get("bugTrackerType") match{
-                case Some(typeName : String) =>
-                  println("TYPE: " + typeName)
-                  typeName match{
-                    case BugTrackerType.BUGZILLA =>{
-                      btCrawler = new BugzillaCrawler(bugTrackerURL)
-                      println("Bugzilla Crawler Start...")
-                      val list = btCrawler.getBugList(btPrjName)
-                      println("Bugzilla Crawler Done.")
-                      println("Total Bugs Grabbed: " + list.count((BugEntity) => true))
-                    }
-                    case BugTrackerType.JIRA =>
-                    case BugTrackerType.NONE =>
-                    case _ =>
-                  }
-                case _ =>
-              }
+              case BugTrackerType.JIRA =>
+              case BugTrackerType.NONE =>
+              case _ =>
+            }
             })
+
 
             actors.Scheduler.execute({
               //TODO run whole download from svn, mse .... etc
-              val formatter = new SimpleDateFormat("yyyy-MM-dd_hh:mm:ss")
               try{
 
-                var prjName = ""
-                map.get("projectName") match{
-                  case Some(s : String) => prjName = s.replaceAll("[\\s]", "_")
-                  case _ =>
-                }
+                val inFamix = new InFamixWrapper(
+                  new File(analysisPath + File.separator + "/inFamix/MacOS").getCanonicalPath,
+                            FamixLanguage.JAVA.getId)
 
-
-
-                var repository = ""
-                  map.get("projectRepo") match{
-                  case Some(s: String) => repository = s.replaceAll("[\\s]", "_")
-                  case _ =>
-                }
-
-                println("REPO:" + repository)
-                println("PROJECT:" + prjName)
-
-                val inFamix = new InFamixWrapper("./inFamix/MacOS", FamixLanguage.JAVA.getId)
-
-
-                map.get("repoType") match{
-                  case Some(repoType : String) => {
-                    repoType match{
-                      case RepositoryType.SVN => {
-                         val crawler = new SVNCrawler(
-                                    repository,
-                                    prjName,
-                                    "/Users/Ponzanelli/Documents/workspace/Genesis-Scala/"+prjName,
-                                    "/Users/Ponzanelli/Documents/workspace/Genesis-Scala/mse",
-                                    inFamix,
-                                    null,
-                                    ".java")
-                          crawler.onSourceParsingCompleteDelegates+=onParsingCompleted
-                          crawler.crawl(1, 100)
-                      }
-                      case RepositoryType.CVS =>
-                      case RepositoryType.GIT =>
-                      case _ =>
-                    }
-
+                repoType match{
+                  case RepositoryType.SVN => {
+                     val crawler = new SVNCrawler(
+                                repository,
+                                prjName,
+                                new File(analysisPath + File.separator + prjName).getCanonicalPath,
+                                new File(analysisPath + File.separator + "mse").getCanonicalPath,
+//                                "/Users/Ponzanelli/Documents/workspace/Genesis-Scala/"+prjName,
+//                                "/Users/Ponzanelli/Documents/workspace/Genesis-Scala/mse",
+                                inFamix,
+                                null,
+                                ".java")
+                      crawler.onSourceParsingCompleteDelegates+=onParsingCompleted
+                      crawler.crawl(11, 100)
                   }
-
+                  case RepositoryType.CVS =>
+                  case RepositoryType.GIT =>
                   case _ =>
                 }
-
-
               }catch{
                 case e: SVNException => println(e)
                 case anyEx : Exception => anyEx.printStackTrace()
@@ -144,7 +134,7 @@ class Service(val port: Int) {
 
         }
         client.close();
-
+        }
       }
       server.close();
     }
@@ -156,24 +146,31 @@ class Service(val port: Int) {
     println("Shutting down..")
   }
 
-  def onParsingCompleted(revisionInfo: RevisionEntity, mseFile : File): Unit = {
+  def onParsingCompleted(affectedRevisions: ListBuffer[RevisionEntity],
+                         currentRevisionNumber : Int, projectName : String,
+                         mseFile : File) {
 	  import scala.io._
 
 	  val source = Source.fromFile(mseFile)
-    val content = source.mkString
+    val mse = source.mkString
     source.close()
-	  val res = MSEParser.parse(content) match {
-	    case Some(res) =>
-	      val mutator =  new RevisionInfoMutator(revisionInfo)
-	      mutator.mutate(res)
-        (new BreadthFirstNavigator).walkModel(res,new ModelPrinter, Some(mutator.getSelection()))
-	    case _ =>
 
-	  }
-	}
+    val res = new ModelGenerator(projectName).generateFromString(mse)
+    res match {
+      case Some(node) => {
+        var graph = new GraphExtractor().extractGraph(node)
+        //println(graph);
+        var mongo = new MongoDBWrapper("127.0.0.1", 4321, "genesis_db")
+        mongo.save(graph, projectName, currentRevisionNumber)
+
+        //new BreadthFirstNavigator().walkModel(node, new ModelPrinter());
+      }
+      case _ =>
+    }
+  }
 
 
-  def asMap(stream: InputStream): Option[HashMap[String, String]] = {
+  private def asMap(stream: InputStream): Option[HashMap[String, String]] = {
     val br: BufferedReader = new BufferedReader(new InputStreamReader(stream));
     val sb: StringBuilder = new StringBuilder();
     var line: String = "";
@@ -196,6 +193,31 @@ class Service(val port: Int) {
     }
     Some(map);
 
+  }
+
+
+  private def createAnalysisFolder(projectName : String, inFamixPath : String) : String = {
+    //Create Folder
+    val analysisFolder = new File(defaultAnalysisFolder + File.separator + projectName)
+    analysisFolder.mkdirs()
+
+    //Copy inFamix
+    Runtime.getRuntime.exec("cp -r " + inFamixPath + " " + analysisFolder.getCanonicalPath)
+
+    //Create Mse folder
+    new File(analysisFolder.getCanonicalPath + File.separator + "mse").mkdirs()
+
+    analysisFolder.getCanonicalPath
+  }
+
+  private def extractParameterValue(map : HashMap[String, String],  key : String) : String ={
+    var value = ""
+    map.get(key) match{
+      case Some(s : String) => value = s.replaceAll("[\\s]", "_")
+      case _ =>
+    }
+
+    value
   }
 
 
