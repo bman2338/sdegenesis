fe1ed6c2562fa3284ea87cc72346de6bf5ff6606
patrick.zulian <patrick.zulian@142c74db-416a-1121-bd68-d796f0770063>
2011-12-10 23:31:57 +0000

    Riconfigured ports and host parameters to work in local computer, everyone has now to be able to run everything on its machine. Install the js stuff with mac ports (nodejs, npm) and with npm an make your own run_local_<yourname>.sh

--- scripts/shellscripts/run_local_pat.sh
+++ scripts/shellscripts/run_local_pat.sh
@@ -1,6 +1,11 @@
+#export JAVA_OPTS=-Xmx1024M
+
+#scala ../../out/production/IdeaProjects/scala/ch/usi/inf/genesis/server/Service.class
 cd /Users/patrick/IdeaProjects/website
 mongod --dbpath=/Users/patrick/IdeaProjects/database --port=8888  & node server.js
+
 ps aux | grep node
 ps aux | grep mongod
+#ps aux | grep Service
 
 cd /Users/patrick/IdeaProjects/scripts/shellscripts
