14ecc3848a75c330ba53393386b180f2cf99d7b9
remo.lemma <remo.lemma@gmail.com>
2011-12-11 16:52:04 +0000

    Adding visualizations register

---
+++ scripts/js/src/VisualizationRegister.js
@@ -0,0 +1,25 @@
+function VisualizationsRegister () {
+	var register = []
+
+	this.addVisualization = function (vis) {
+		register.push(vis);
+	}
+
+	this.getVisualizations = function (data) {
+		var results = [];
+		for (var i = 0; i < register.length; ++i) {
+			var vis = register[i];
+			var toAdd = true;
+			for (var j = 0; j < data.length; ++j) {
+				if (!vis.allows(data[j])) {
+					toAdd = false;
+					break;
+				}
+			}
+			if (toAdd)
+				results.push(vis);
+		}
+		return results;
+	}
+	return this;
+}
