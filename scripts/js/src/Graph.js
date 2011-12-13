289523e7fa505403156d877adee7d5ab3c0abbcc
patrick.zulian <patrick.zulian@142c74db-416a-1121-bd68-d796f0770063>
2011-12-13 11:23:35 +0000

    Now Graph.js supports graphs with mixed relation types

--- scripts/js/src/Graph.js
+++ scripts/js/src/Graph.js
@@ -26,24 +26,35 @@ function binarySearch(array, uniqueId, less, eq, fromIndex){
 	return array.length;
 }
 
-	function concat(listOfLists) {
-		if(listsOfLists.length == 0) {
-			return [];
-		}
-		
-		if(listsOfList.length == 1) {
-			return listOfLists[0];
-		}
-		
-		var res = [];
-		for(var ll in listOfLists) {
-			var list = listOfLists[ll];
-			for(var l in list) {
-				res.push(list[l]);
-			}
-		}
-		return res;
+
+function objToArray(obj) {
+	var arr = [];
+	
+	for(var key in obj) {
+		arr.push(obj[key]);
 	}
+	
+	return arr;
+}
+
+function concat(listOfLists) {
+	if(listsOfLists.length == 0) {
+		return [];
+	}
+
+	if(listsOfList.length == 1) {
+		return listOfLists[0];
+	}
+
+	var res = [];
+	for(var ll in listOfLists) {
+		var list = listOfLists[ll];
+		for(var l in list) {
+			res.push(list[l]);
+		}
+	}
+	return res;
+}
 
 
 var genesis = { Graph : {}};
@@ -83,7 +94,7 @@ genesis.Graph.create = function(nodes, edges) {
 	};
 
 	var graph = {
-		
+
 		//public functions
 		initialize : function(nodes, edges) {
 			this.nodes = nodes.sort(nodeComparator);
@@ -128,27 +139,27 @@ genesis.Graph.create = function(nodes, edges) {
 			}
 
 		},
-			//TODO test it and eventually subsitute the code on the above method
-			getOneToOneEdgesFromAdjList : function(adjLists) {
-				var relation =  adjLists;
+		//TODO test it and eventually subsitute the code on the above method
+		getOneToOneEdgesFromAdjList : function(adjLists) {
+			var relation =  adjLists;
 
-				if(relation) {
-					var oneToOneEdges = [];
-					for(var adj in relation) {
-						var adjList = relation[adj];
-						var from = adjList.from;
-						for(var e in adjList.to) {
-							var to = adjList.to[e];
-							oneToOneEdges.push({ "from": from, "to": to }); 
-						}
+			if(relation) {
+				var oneToOneEdges = [];
+				for(var adj in relation) {
+					var adjList = relation[adj];
+					var from = adjList.from;
+					for(var e in adjList.to) {
+						var to = adjList.to[e];
+						oneToOneEdges.push({ "from": from, "to": to }); 
 					}
-					return oneToOneEdges;
-				} else {
-					return null;
 				}
-			},
+				return oneToOneEdges;
+			} else {
+				return null;
+			}
+		},
 
-		
+
 		getRelation : function(relationName) {
 			return this.edges[relationName];
 		},
@@ -159,11 +170,11 @@ genesis.Graph.create = function(nodes, edges) {
 
 		},
 
-		getNodeList : function(adjList) {
+		getNodeList : function(nodeIdList) {
 			var nodeList = [];
-			if(adjList) {
-				for(var i = 0; i < adjList.length; i++) {
-					var node = this.getNodeFromId(adjList[i]);
+			if(nodeIdList) {
+				for(var i = 0; i < nodeIdList.length; i++) {
+					var node = this.getNodeFromId(nodeIdList[i]);
 					if(node)
 					nodeList.push(node);
 				}
@@ -176,10 +187,10 @@ genesis.Graph.create = function(nodes, edges) {
 			this.oneToOneEdges = null;
 			this.nodesByType = null;
 		},
-        
 
-        // select(node) -> boolean
-        getNodeSelection : function(select) {
+
+		// select(node) -> boolean
+		getNodeSelection : function(select) {
 			var nodeList = [];
 			for(var n in this.nodes) {
 				var node = this.nodes[n];
@@ -187,45 +198,45 @@ genesis.Graph.create = function(nodes, edges) {
 					nodeList.push(node);
 				}
 			}
-			
+
 			return nodeList;
-        },
-        
-        
-                
-        getNodeFromListFromId : function(nodeList, uniqueId)  {
-            return ifInRangeGet(nodeList, binarySearch(nodeList, uniqueId, nodeLess, nodeEquals));
-        
-        },
+		},
+
+
+
+		getNodeFromListFromId : function(nodeList, uniqueId)  {
+			return ifInRangeGet(nodeList, binarySearch(nodeList, uniqueId, nodeLess, nodeEquals));
+
+		},
 
 		getSubgraphAux : function(adjList, adjLists, relation, connected) {
 			if(!adjList) {
 				return;
 			}
-			
+
 			var toCheck = [adjList];
-			
+
 			while (toCheck.length != 0) {
 				var check = toCheck[0];
 				toCheck = toCheck.slice(1);
-				
+
 				if (!connected[check.from]) {
 					connected[check.from] = true;
 					adjLists.push(check);
-					
+
 					for (var toAdj in check.to) {
 						var toAdjList = this.getAdjList(relation,check.to[toAdj]);
 						if (toAdjList != null) {
 							toCheck.push(toAdjList);
 						} else {
-                            connected[check.to[toAdj]] = true;
-                        }
+							connected[check.to[toAdj]] = true;
+						}
 					}
 				}
 			}
-			
+
 		},
-        
+
 
 		/**
 		* opt.center : the node from which the graph is built.
@@ -234,222 +245,259 @@ genesis.Graph.create = function(nodes, edges) {
 		* else it will return an array of adjLists.
 		* return { nodes:  { relName : [ nodes* ] },  (edges|adjLists) : { relName : [ (edges|adjLists)* ] }  }
 		*/ 
-        getSubgraph : function(opt) {
+		getSubgraph : function(opt) {
 			if(!opt.center) return null;
-			
+
 			var center = opt.center;
 			var adjLists = {};
 			var nodes = {};
-			
+
 			for(var r in this.edges) {
 				var relation = this.edges[r];
 				if(opt.selectRelation(r)) {
 					adjLists[r] = [];
 					var connected = {};
-					
+
 					var adjList = this.getAdjList(relation, center.uniqueId);
 					this.getSubgraphAux(adjList, adjLists[r], relation, connected);
 					nodes[r] = this.getNodesFromIdSet(connected);
 				}
 			}
-			
+
+			var ret = { "nodes" : nodes, "adjLists" : adjLists };
+			var edgeSelector = "adjLists";
+
 			if(opt.oneToOneEdges) {
 				var edges = {};
 				for(var r in adjLists) {
 					edges[r] = this.getOneToOneEdgesFromAdjList(adjLists[r]);
 				}
-				
-				return { "nodes" : nodes, "edges" : edges };
-				
+				var ret =  { "nodes" : nodes, "edges" : edges };
+				edgeSelector = "edges";
+
+			}
+
+			if(opt.collapse) {
+				ret = this.fromMultiGraphToGraph(ret, edgeSelector);
+			}
+
+			return ret;
+		},
+
+
+		fromMultiGraphToGraph : function(multiGraph, edgeSelector) {
+			var multiNodes = multiGraph.nodes;
+			var multiEdges = multiGraph[edgeSelector];
+
+			var nodes = {};
+			for(var ns in multiNodes) {
+				var relation = multiNodes[ns];
+				for(var n in relation) {
+					var node = relation[n];
+					nodes[node.uniqueId] = node;
+				}
 			}
 			
-			return { "nodes" : nodes, "adjLists" : adjLists };
+	
+
+
+			var edges = [];
+			for(var es in multiEdges) {
+				var relation = multiEdges[es];
+				for(var e in relation) {
+					var edge = relation[e];
+					edge.type = es;
+					edges.push(edge);
+				}
+			}
+
+			var ret =  { "nodes" : objToArray(nodes) };
+			ret[edgeSelector] = edges;
+			return ret;
 		},
-		
-		
+
+
 		getNodesFromIdSet: function(idSet) {
 			var nodes = [];
 			for(var id in idSet) {
-                var node = this.getNodeFromId(id);
-                if(node) {
-                    nodes.push(node);
-                }
+				var node = this.getNodeFromId(id);
+				if(node) {
+					nodes.push(node);
+				}
 			}
 			return nodes;
 		},
-        
-     
-        /**
-        * opt.selectEdge : (node1, node2) -> boolean
-        * opt.selectRelation : (relationName) -> boolean
-        * opt.selectNode : (node) -> boolean
-        * opt.removeUnconnected : Boolean
-        * returns { nodes: [node*], edges: [ { rel : [ {from, to}* ] }* ] }
-        *
-        * Example usage of getSelection(opt);
-            
-        	getSelection({
-                removeUnconnected : true,
-                selectRelation : function(rel) { return rel == anExampleRelationName; },
-                selectNode : function(node) {   return node.properties.ElementType == anExampleNodeType; },
-                selectEdge : function(nodeFrom, nodeTo) { return true; }
-			});
-        */
-        getSelection : function(opt) {
-            
-            if(!opt.selectEdge) {
-                opt.selectEdge = function(x, y) { return true; };
-            } 
-            if(!opt.selectRelation) {
-                opt.selectRelation = function(x) { return true; };
-            } 
-            if(!opt.selectNode) {
-                opt.selectNode = function(x) { return true; };
-            } 
-            
-            if(opt.removeUnconnected) {
-                opt.connected = {};
-            }
-            
-            var nodeSelection = [];
-            for(var n in this.nodes) {
-                var node = this.nodes[n];
-                if(opt.selectNode(node)) {
-                    nodeSelection.push(node);
-                }
-            }
-            
-        
-        
-        
-           edgeSelection = [];
-           for(var rel in this.edges) {
-                if(opt.selectRelation(rel)) {
-                    edgeSelection[rel] = (this.getEdgeSelectionFromRelation(opt, nodeSelection, this.edges[rel]));
-                }
-           }
-           
-           if(opt.removeUnconnected) {
-            var temp = nodeSelection;
-            nodeSelection = [];
-            for(var n in temp) {
-                var id = temp[n].uniqueId;
-                if(opt.connected[id]) {
-                    nodeSelection.push(temp[n]);
-                }
-            }
-            
-           }
-           opt.connected = {};
-            return { "nodes" : nodeSelection, "edges" : edgeSelection };
-        },
-        
 
 
-        getEdgeSelectionFromRelation : function(opt, nodeSelection, relation) {
-            selection = [];
-            for(var r in relation) {
-                var adjList = relation[r];
-                var from = adjList.from;
-                var fromNode = this.getNodeFromListFromId(nodeSelection, from);
-                if(!fromNode) {
-                    continue;
-                }
-                
-                for(var adjTo in adjList.to) {
-                    var to = adjList.to[adjTo];
-                    var toNode = this.getNodeFromListFromId(nodeSelection, to);
-                    if(!toNode) {
-                        continue;     
-                    }
-                    
-                   
-                    
-                    if(opt.selectEdge(fromNode, toNode)) {
-                        selection.push({ "from" : from, "to" : to });
-                         if(opt.removeUnconnected) {
-                            opt.connected[from] = true;
-                            opt.connected[to] = true;
-                    }
-                    }
-                }
-            }   
-            return selection;
-        },
-        
-        
-		getNodesByType : function(type) {
-			var nodeList = [];
-			if(this.nodesByType && this.nodesByType[type]) {
-				return this.nodesByType[type];
+		/**
+		* opt.selectEdge : (node1, node2) -> boolean
+		* opt.selectRelation : (relationName) -> boolean
+		* opt.selectNode : (node) -> boolean
+		* opt.removeUnconnected : Boolean
+		* returns { nodes: [node*], edges: [ { rel : [ {from, to}* ] }* ] }
+		*
+		* Example usage of getSelection(opt);
+
+		getSelection({
+		removeUnconnected : true,
+		selectRelation : function(rel) { return rel == anExampleRelationName; },
+		selectNode : function(node) {   return node.properties.ElementType == anExampleNodeType; },
+		selectEdge : function(nodeFrom, nodeTo) { return true; }
+		});
+		*/
+		
+		getSelection : function(opt) {
+
+			if(!opt.selectEdge) {
+				opt.selectEdge = function(x, y) { return true; };
+			} 
+			if(!opt.selectRelation) {
+				opt.selectRelation = function(x) { return true; };
+			} 
+			if(!opt.selectNode) {
+				opt.selectNode = function(x) { return true; };
+			} 
+
+			if(opt.removeUnconnected) {
+				opt.connected = {};
 			}
 
+			var nodeSelection = [];
 			for(var n in this.nodes) {
 				var node = this.nodes[n];
-				if(this.getNodeType(node) == type) {
-					nodeList.push(node);
-				}
-			}
-			if(!this.nodesByType) {
-				this.nodesByType = {};
-			}
-
-			this.nodesByType[type] = nodeList;
-			return nodeList;
-		},
-
-		getNodeType : function(node) {
-			return node.properties.ElementType;
-		},
-
-		getNodeFromId : function(uniqueId){
-			return ifInRangeGet(this.nodes, binarySearch(this.nodes, uniqueId, nodeLess, nodeEquals));
-		},
-		
-		getNodes: function () {
-			return nodes;
-		},
-
-		getAdjLists: function () {
-			return edges;
-		},
-
-		getSubtreeByRelationName : function(relationName, nodeId) {
-			var relation = this.getRelation(relationName);
-			if(!relation)	
-			{
-				return null;
-			}
-			var node = this.getNodeFromId(nodeId);
-			if(!node) {
-				return null;
-			}	
-
-
-			return this.getSubtree(relation, node);
-		},
-
-		getSubtree : function(relation, node) {
-			var adjList = this.getAdjList(relation, node.uniqueId);
-			var nodeList = [];
-			if (adjList) {
-				nodeList  = this.getNodeList(adjList.to);
-			}
-
-			var childNodes = [];
-			for(var i = 0; i < nodeList.length; i++) {
-				if(nodeList[i] && nodeList[i].properties.name){
-					childNodes.push(this.getSubtree(relation, nodeList[i]));
+				if(opt.selectNode(node)) {
+					nodeSelection.push(node);
 				}
 			}
 
-			var obj = { name: node.properties.name, properties: node.properties, metrics: node.metrics };
-
-			if(childNodes.length != 0)  {
-				obj.children = childNodes
+			edgeSelection = [];
+			for(var rel in this.edges) {
+				if(opt.selectRelation(rel)) {
+					edgeSelection[rel] = (this.getEdgeSelectionFromRelation(opt, nodeSelection, this.edges[rel]));
+				}
 			}
-			return obj;
+
+			if(opt.removeUnconnected) {
+				var temp = nodeSelection;
+				nodeSelection = [];
+				for(var n in temp) {
+					var id = temp[n].uniqueId;
+					if(opt.connected[id]) {
+						nodeSelection.push(temp[n]);
+					}
+				}
+
+			}
+			opt.connected = {};
+			return { "nodes" : nodeSelection, "edges" : edgeSelection };
 		},
 
-		}; return graph.initialize(nodes, edges)
-	}
+
+
+	getEdgeSelectionFromRelation : function(opt, nodeSelection, relation) {
+		selection = [];
+		for(var r in relation) {
+			var adjList = relation[r];
+			var from = adjList.from;
+			var fromNode = this.getNodeFromListFromId(nodeSelection, from);
+			if(!fromNode) {
+				continue;
+			}
+
+			for(var adjTo in adjList.to) {
+				var to = adjList.to[adjTo];
+				var toNode = this.getNodeFromListFromId(nodeSelection, to);
+				if(!toNode) {
+					continue;     
+				}
+
+
+
+				if(opt.selectEdge(fromNode, toNode)) {
+					selection.push({ "from" : from, "to" : to });
+					if(opt.removeUnconnected) {
+						opt.connected[from] = true;
+						opt.connected[to] = true;
+					}
+				}
+			}
+		}   
+		return selection;
+	},
+
+
+	getNodesByType : function(type) {
+		var nodeList = [];
+		if(this.nodesByType && this.nodesByType[type]) {
+			return this.nodesByType[type];
+		}
+
+		for(var n in this.nodes) {
+			var node = this.nodes[n];
+			if(this.getNodeType(node) == type) {
+				nodeList.push(node);
+			}
+		}
+		if(!this.nodesByType) {
+			this.nodesByType = {};
+		}
+
+		this.nodesByType[type] = nodeList;
+		return nodeList;
+	},
+
+	getNodeType : function(node) {
+		return node.properties.ElementType;
+	},
+
+	getNodeFromId : function(uniqueId){
+		return ifInRangeGet(this.nodes, binarySearch(this.nodes, uniqueId, nodeLess, nodeEquals));
+	},
+
+	getNodes: function () {
+		return nodes;
+	},
+
+	getAdjLists: function () {
+		return edges;
+	},
+
+	getSubtreeByRelationName : function(relationName, nodeId) {
+		var relation = this.getRelation(relationName);
+		if(!relation)	
+		{
+			return null;
+		}
+		var node = this.getNodeFromId(nodeId);
+		if(!node) {
+			return null;
+		}	
+
+
+		return this.getSubtree(relation, node);
+	},
+
+	getSubtree : function(relation, node) {
+		var adjList = this.getAdjList(relation, node.uniqueId);
+		var nodeList = [];
+		if (adjList) {
+			nodeList  = this.getNodeList(adjList.to);
+		}
+
+		var childNodes = [];
+		for(var i = 0; i < nodeList.length; i++) {
+			if(nodeList[i] && nodeList[i].properties.name){
+				childNodes.push(this.getSubtree(relation, nodeList[i]));
+			}
+		}
+
+		var obj = { name: node.properties.name, properties: node.properties, metrics: node.metrics };
+
+		if(childNodes.length != 0)  {
+			obj.children = childNodes
+		}
+		return obj;
+	},
+
+	}; return graph.initialize(nodes, edges)
+}
