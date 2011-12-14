1e17a8ef79bb870945b6aa4d460a399b4bc96fbc
patrick.zulian <patrick.zulian@142c74db-416a-1121-bd68-d796f0770063>
2011-12-14 16:25:02 +0000

    added calendar view  sketch

---
+++ scripts/js/src/calendar/calendar.js
@@ -0,0 +1,105 @@
+//Date,Open,High,Low,Close,Volume,Adj Close
+
+function plotHistoryCalendar(historyDates, historyData, visModel, augmentationCallback) {
+	var m = [19, 20, 20, 19], // top right bottom left margin
+	w = 960 - m[1] - m[3], // width
+	h = 136 - m[0] - m[2], // height
+	z = 17; // cell size
+
+	var day = d3.time.format("%w"),
+	week = d3.time.format("%U"),
+	percent = d3.format(".1%"),
+	format = d3.time.format(getHistoryDateFormatString());
+
+	var color = d3.scale.quantize()
+	.domain([-.05, .05])
+	.range(d3.range(9));
+
+	var svg = d3.select("#chart").selectAll("svg")
+	.data(d3.range(1998, 2011))
+	.enter().append("svg:svg")
+	.attr("width", w + m[1] + m[3])
+	.attr("height", h + m[0] + m[2])
+	.attr("class", "RdYlGn")
+	.append("svg:g")
+	.attr("transform", "translate(" + (m[3] + (w - z * 53) / 2) + "," + (m[0] + (h - z * 7) / 2) + ")");
+
+	svg.append("svg:text")
+	.attr("transform", "translate(-6," + z * 3.5 + ")rotate(-90)")
+	.attr("text-anchor", "middle")
+	.text(String);
+
+	var rect = svg.selectAll("rect.day")
+	.data(function(d) { 
+		return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); 
+	})
+	.enter().append("svg:rect")
+	.attr("class", "day")
+	.attr("width", z)
+	.attr("height", z)
+	.attr("x", function(d) { return week(d) * z; })
+	.attr("y", function(d) { return day(d) * z; });
+
+	svg.selectAll("path.month")
+	.data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+	.enter().append("svg:path")
+	.attr("class", "month")
+	.attr("d", monthPath);
+
+
+	var getAuthor = function(d) {
+		var fd = format(d);
+		var array = historyData[fd];
+		if(array)
+			return array[0].author;
+		return null;
+	};
+
+	plotHistory = function(historyDates, historyData) {
+
+		var data = d3.nest()
+		.key(function(d) { 
+			return d; 
+		})
+		.rollup(function(d) { 
+			return  d[0];  
+			})//(d[0].Close - d[0].Open) / d[0].Open; })
+			.map(historyDates);
+
+			rect
+			.attr("class", function(d) { 
+				return "day q" + color(data[format(d)]) + "-9"; 
+			})
+			.style("fill", function(d) { 
+				var auth = getAuthor(d); 
+				if(auth)
+					return "steelblue";
+				return "white";
+			})
+			.append("svg:title")
+			.text(function(d)  { 
+				var auth = getAuthor(d);
+				if(auth)
+				return auth;
+				return (d = format(d)) + (d in data ? ": " + percent(data[d]) : ""); 
+
+			});
+		};
+
+
+		plotHistory(historyDates, historyData); 
+
+		function monthPath(t0) {
+			var t1 = new Date(t0.getUTCFullYear(), t0.getUTCMonth() + 1, 0),
+			d0 = +day(t0), w0 = +week(t0),
+			d1 = +day(t1), w1 = +week(t1);
+			return "M" + (w0 + 1) * z + "," + d0 * z
+			+ "H" + w0 * z + "V" + 7 * z
+			+ "H" + w1 * z + "V" + (d1 + 1) * z
+			+ "H" + (w1 + 1) * z + "V" + 0
+			+ "H" + (w0 + 1) * z + "Z";
+		}
+
+	}
+
+
--- scripts/js/src/cleanHistory.js
+++ scripts/js/src/cleanHistory.js
@@ -18,15 +18,70 @@
 			entry.project = entry.project[0];
 			entry.hasMse = entry.hasMse[0];
 
-			var iter = [ "deletedFiles", "addedFiles", "modifiedFiles" ];
-
-			for(var it in iter) {
-				var list = entry[iter[it]];
-				for(var le in list) {
-					list[le] = list[le][0];
-				}
-			}
-			newHist[e] = entry;
+            newHist[e] = cleanFileOps(entry);
 		}
+        
+        
 		return newHist;
 	};
+
+    function cleanFileOps(entry) {
+         var iter = [ "deletedFiles", "addedFiles", "modifiedFiles" ];
+			for(var it in iter) {
+				var fileOp = entry[iter[it]];
+				cleanFileOp(fileOp);
+			}    
+        return entry;
+    }
+    
+    function cleanFileOp(fileOp) {
+        for(var le in fileOp) {
+					fileOp[le] = fileOp[le][0];
+				}
+    }
+
+	function historyToD3Format(hist) {
+		var data = {};
+		var dates = [];
+		var format = d3.time.format(getHistoryDateFormatString())
+		for(var e in hist) {
+			var entry = hist[e];
+			entry.number = e;
+			var dateStr = getDateStrNoZone(entry.date);
+			
+			
+			var fd = format.parse(dateStr);
+            
+            fd.setMinutes(0);
+            fd.setHours(0);
+            fd.setSeconds(0);
+            
+			var date = format(fd);
+			if(!data[date]) {
+				data[date] = [];
+				dates.push(date);
+			}
+			//TODO make copy
+			data[date].push(entry);
+			
+		}
+		return { "dates": dates, "data" : data };
+	}
+	
+	function getDateStrNoZone(dateStr) {
+		var arr = dateStr.split(" ");
+		var res = "";
+		arr[4] = " ";
+
+		return res.concat(arr);
+	}
+	
+
+	function getHistoryDateFormatString() {
+		return "%a,%b,%d,%X, ,%Y";
+	}
+	
+	function getD3DataFormatString() {
+		return "%Y-%m-%d";
+	}
+	
