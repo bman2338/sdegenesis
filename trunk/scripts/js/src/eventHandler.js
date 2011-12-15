72960bd402625d70c87443b3f3bdbf71a0186abc
masiar88 <masiar88@142c74db-416a-1121-bd68-d796f0770063>
2011-12-15 19:19:46 +0000

    some modifications on the server, the viz and the eventHandler which was buggy

--- trunk/scripts/js/src/eventHandler.js
+++ trunk/scripts/js/src/eventHandler.js
@@ -9,27 +9,28 @@ function EventHandler(){
 	var handlers = {
 		window: {
 			defaults: {
-				keydown: undefined;
-			}
+				keydown: undefined,
+			},
 			values: {
 			}
+		}
 	};
 	return{
 		pause: function(){
 			for(hndlr in handlers){
 				d3.select(hndlr).on(hndlr.values, hndlr.values.handler).remove();
 			}
-		};
+		},
 		
-		resume: function(){};
+		resume: function(){},
 		
-		add: function(object, handler, type){
+		add: function(object, type, handler){
 			if (!handlers[object])
 				handlers[object] = { defaults: {}, values: {} };
 			handlers[object].values[type] = handler;
 			//handlers.push({obj:object, hand:handler, typ:type, running:true});
 			d3.select(object).on(type,handler);
-		};
-		remove: function(){};
+		},
+		remove: function(){},
 	}
 }
