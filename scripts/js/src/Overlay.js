81c5e72f71dd370f610daf943f3952984ca996f6
R.Minelli88 <R.Minelli88@142c74db-416a-1121-bd68-d796f0770063>
2011-12-15 20:54:37 +0000

    Adding authors to the overlay

--- scripts/js/src/Overlay.js
+++ scripts/js/src/Overlay.js
@@ -1,6 +1,6 @@
+var skipElements = ["Revision","Author"];
 var elementsSelected = [];
 var hidden = true;
-//var resultsToAnalyze = [];
 
 function showHide() {
 	if(hidden) {
@@ -18,33 +18,11 @@ function showHide() {
 };
 
 function displayList(divId, elements, callback) {
-	
-	
-      var div = document.getElementById(divId);
-
-	   var tree = div.append("<ul class=\"tree\"></ul>").find('ul');
-	
-		$.each(elements,function(index,element) {
-			tree.append("<li>"+element+"</li>");
-		});
-
-        //while (!item.tagName || item.tagName.toLowerCase() != "li")
-     	//  item = item.previousSibling;
-
-        //item.className += " last";
-
- /*
-	
-	var list = '';
-	$.each(elements,
-		function(index, element) { 
-			if(callback) {
-				list += '<li><a href="#" onClick="javascript:' + callback + '(\'' + element + '\');">' + element + '</a></li>';
-			} else {
-				list += '<li>' + element + '</li>';
-			} 
-		});*/
-	//document.getElementById(divId).innerHTML = list;
+	var div = document.getElementById(divId);
+	var tree = div.append("<ul class=\"tree\"></ul>").find('ul');
+	$.each(elements,function(index,element) {
+		tree.append("<li>"+element+"</li>");
+	});
 }
 
 function drawFunction (vis,analysis,nodes,types) {
@@ -65,20 +43,18 @@ function initialVisualization (analysisId,visualizationId) {
 	if (elementsSelected.indexOf("Revision") == -1) {
 		projects[0][currentProject].getRevision(projects[0][currentProject], projects[0][currentProject].currRev, function(rev){
 			var vis = bootstrapVisualization(analysis,visualizationId,rev.graph);
-			
 			var resultsToAnalyze = [];
-			$.each(elementsSelected,function(index,header) {
-			$("#" + header + "-select option:selected").each(function (index,element) {
-				resultsToAnalyze.push(rev.graph.getNodeFromId(element.value));
-			});
+			$.each(elementsSelected, function(index,header) {
+				$("#" + header + "-select option:selected").each(function (index,element) {
+					resultsToAnalyze.push(rev.graph.getNodeFromId(element.value));
+				});
 			});
 			if (resultsToAnalyze.length == 0)
 				return false;
-			
+	
 			drawFunction(vis,analysis,resultsToAnalyze,elementsSelected);
 		});
-	}
-	else {
+	} else {
 		var history = projects[0][currentProject].history;
 		var vis = bootstrapVisualization(analysis,visualizationId,history);		
 		drawFunction(vis,analysis,[vis.source],elementsSelected);
@@ -89,35 +65,26 @@ function displayAnalysis (divId,elements) {
 	
 	var div = $("#"+divId);
 	div.html("");
-	   var tree = div.append("<ul class=\"tree\"></ul>").find('ul');
-	
-		$.each(elements,function(index,element) {
-			var list = null;
-			if (index == elements.length-1)
-				list = tree.append("<li class=\"last top-analysis\">"+element.name+"</li>").find('li');
-			else
-				list = tree.append("<li>"+element.name+"</li>").find('li');
-			var innerTree = list.append("<ul></ul>").find('ul');
-			var visz = element.getVisualizations(nodes);
-			var analysisIndex = index;
-			$.each(visz,function(index,element) {
-				var clickFun = "javascript:initialVisualization(" + analysisIndex + "," + index +")"
-				if (index == visz.length-1)
-					innerTree.append("<li class=\"last\" onclick=\"" + clickFun + "\">"+element.name+"</li>");
-				else
-					innerTree.append("<li class=\"inner-viz\" onclick=\"" + clickFun + "\">"+element.name+"</li>");
-			});				
-		});
-}
+	var tree = div.append("<ul class=\"tree\"></ul>").find('ul');
 
-// function displayMultiple(formId, elements) {
-// 	var checkboxes = '';
-// 	$.each(elements,
-// 		function(index, element) {
-// 			checkboxes += '<input type="checkbox" value=\"'+element+'\" value=\"'+element+'\" />'+element+'<br/>'; 
-// 		});
-// 	document.getElementById(formId).innerHTML = checkboxes;
-// }
+	$.each(elements, function(index,element) {
+		var list = null;
+		if (index == elements.length-1)
+			list = tree.append("<li class=\"last top-analysis\">"+element.name+"</li>").find('li');
+		else
+			list = tree.append("<li>"+element.name+"</li>").find('li');
+		var innerTree = list.append("<ul></ul>").find('ul');
+		var visz = element.getVisualizations(nodes);
+		var analysisIndex = index;
+		$.each(visz,function(index,element) {
+			var clickFun = "javascript:initialVisualization(" + analysisIndex + "," + index +")"
+			if (index == visz.length-1)
+				innerTree.append("<li class=\"last\" onclick=\"" + clickFun + "\">"+element.name+"</li>");
+			else
+				innerTree.append("<li class=\"inner-viz\" onclick=\"" + clickFun + "\">"+element.name+"</li>");
+		});				
+	});
+}
 
 function displayMultiple(divId, elements) {
 	var options = '<select id=\"node-selection\" multiple="multiple" size="4">';	
@@ -126,7 +93,7 @@ function displayMultiple(divId, elements) {
 		function(index, element) {
 			options += '<option value=\"'+element.name+'\">' + element.value + '</option>'; 
 		});
-		
+	
 	options += '</select>';	
 		
 	document.getElementById(divId).innerHTML = options;
@@ -134,9 +101,6 @@ function displayMultiple(divId, elements) {
 
 function updateResults() { 
 	elementsSelected = [];
-    // $('#nodes :checked').each(function() {
-    //   elementsSelected.push($(this).val());
-    // });
 
 	$("#node-selection option:selected").each(function () {
       elementsSelected.push($(this).val());
@@ -151,12 +115,12 @@ function showResults(elements) {
 	var results = {};	
 	
 	projects[0][currentProject].getRevision(projects[0][currentProject], projects[0][currentProject].currRev, function(rev){
-		
+				
 		var shouldSkip = [];
 				
 		$.each(elements,
 			function(index, node) {
-				if (node == "Revision") {
+				if (skipElements.indexOf(node) != -1) {
 					shouldSkip.push(index);
 				}
 				else {
@@ -191,34 +155,13 @@ function showResults(elements) {
 						if (shouldSkip.indexOf(indexx) == -1) {
 							table += '<option value=\"'+element.uniqueId+'\">' + element.properties.name + '</option>';
 						}
-				});
+					});
 				table += '</td>';
 				}
 			});
-		
-		/*$.each(elementsSelected,
-				function(index, header){
-					alert(header)
-					if (shouldSkip.indexOf(index) == -1) {
-					//setup the trigger
-					$("#" + header + "-select").change(function () {
-						var str = "";
-						$("#" + header + "-select option:selected").each(function () {
-							alert("ASD");
-						});
-					}).trigger('change');
-					}
-				});*/
-				
-				table += '</tr></table>';	
-				resultDiv.innerHTML = table;
+		table += '</tr></table>';	
+		resultDiv.innerHTML = table;
 	});
-	
-/*	resultsToAnalyze = [];
-	for (var key in results) {
-		var res = results[key];
-		resultsToAnalyze.push(res);
-	}*/
 }
 
 function showAnalysis(analysis) {
