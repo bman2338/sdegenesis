069aada9328ccfa0e844d9ce57dc918df98cfdd0
metz85 <metz85@gmail.com>
2011-12-15 22:37:19 +0000

    modified tooltips on tree.js

--- scripts/js/src/displayInfo.js
+++ scripts/js/src/displayInfo.js
@@ -1,27 +1,26 @@
 function createInfo(d){
 	var result = "";
 	if(d.properties){
-        result +="<div id='dispProperties'>";
+        result +="";
         for(var x in d.properties){
             result += x;
             result += ": ";
             result += d.properties[x];	
-            result += " ";
+            result += "<br>";
         }
-        result += "</div>";
+        result += "";
     }
     
    
     if(d.metrics){
-        result +="<div id='dispMetrics'>";
         
         for(var x in d.metrics){
             result += x;
             result += ": ";
             result += d.metrics[x];	
-            result += " ";
+            result += "<br>";
         }
-        result += "</div>";
+        result += "";
     }
     
 	return result;
