f97e4dd18b40a9e371b68eb4a136731c494adfd0
masiar88 <masiar88@142c74db-416a-1121-bd68-d796f0770063>
2011-12-15 18:44:46 +0000

    added absolute paths to imgs and imports because relative paths don't work well with node/jade

--- trunk/website/server.js
+++ trunk/website/server.js
@@ -21,6 +21,8 @@ var express = require('express')
 var url = require('url')
   , fs = require ('fs');
 
+var cleaner = require('./js/cleanHistoryServerSide');
+
 var mongoskin = mongo.db("localhost:8888/genesis_db");
 
 app.listen(8079);
@@ -84,6 +86,7 @@ app.post('/login', function (req, res) {
 					console.log(openProjs[i]);
 					//if it's not in usr projects, add it
 					if(checkProjects(usr.projects, openProjs[i])){
+						openProjs[i].defaultShared = true;
 						usr.projects.push(openProjs[i]);
 					}
 				}
@@ -195,13 +198,33 @@ app.get('/logout', function(req, res){
 app.post('/addProject', function(req, res){
 	var projectName = req.body.project.name;
 	var projectrepo = req.body.project.repo;
-	var repoType = req.body.project.repoType.toLowerCase();
-	var projectBugTracker = req.body.project.bugtracker;
-	var bugtrackerType = req.body.project.btType.toLowerCase();
-	var projectNameBugtracker = req.body.project.projectnamebugtracker;
+	var repoType = req.body.project.repoType.toLowerCase() || "";
+	var projectBugTracker = req.body.project.bugtracker || "";
+	var bugtrackerType = req.body.project.btType.toLowerCase() || "";
+	var projectNameBugtracker = req.body.project.projectnamebugtracker || "";
+	var fromRev = req.body.project.fromRev;
+	var toRev = req.body.project.toRev || "";
+	var stepRev = req.body.project.stepRev;
+	var repoUsername = req.body.project.repoUsername || "";
+	var repoPass = req.body.project.repoPassword || "";
+	var BTUsername = req.body.project.btUsername || "";
+	var BTPass = req.body.project.btPassword || "";
 	
 	mongo.db("localhost:8888/genesis_db").collection("projects").find({name: projectName}).toArray(function(err, projects){
 		console.log("prima del for con username " + req.session.username);
+		console.log("RECEIVED: projectName> "+ projectName + 
+		"\n projectRepo> " + projectrepo + 
+		"\n repoType> " + repoType + 
+		"\n projectBugTracker> " + projectBugTracker + 
+		"\n bugTrackerType> " + bugtrackerType + 
+		"\n projectBugTrackerName> " + projectNameBugtracker +
+		"\n from> " + fromRev +
+		"\n to> " + toRev + 
+		"\n step> " + stepRev + 
+		"\n repoUsername> " + repoUsername + 
+		"\n repoPass> " + repoPass +
+		"\n btUsername> " + BTUsername +
+		"\n btPassword> " + BTPass);
 		var found = false;
 		for(var i = 0; i < projects.length; i++){
 			if(projects[i].name == projectName && projects[i].username == req.session.username){
@@ -214,7 +237,7 @@ app.post('/addProject', function(req, res){
 				//make the scala backend do something
 				//FOR NOW ITS COMMENTED BECAUSE THE SERVICE IS NOT ON THE SERVER
 				var net = require('net');
-				var client = net.createConnection(6969, 'localhost');
+				var client = net.createConnection(6969, '172.16.224.88');
                 
                 client.write(
 					"projectName> "+ projectName + 
@@ -222,7 +245,14 @@ app.post('/addProject', function(req, res){
 					"\n repoType> " + repoType + 
 					"\n projectBugTracker> " + projectBugTracker + 
 					"\n bugTrackerType> " + bugtrackerType + 
-					"\n projectBugTrackerName> " + projectNameBugtracker);
+					"\n projectBugTrackerName> " + projectNameBugtracker +
+					"\n from> " + fromRev +
+					"\n to> " + toRev + 
+					"\n step> " + stepRev + 
+					"\n repoUsername> " + repoUsername + 
+					"\n repoPass> " + repoPass +
+					"\n btUsername> " + BTUsername +
+					"\n btPassword> " + BTPass);
 				client.end();
 				
 				//add the new project to the db
@@ -230,6 +260,7 @@ app.post('/addProject', function(req, res){
 					name : projectName,
 					status: "updated",
 					revisions: [],
+					open: 0,
 				};
 				mongoskin.collection("projects").insert(projToAdd, function(err){
 					if(err){
@@ -267,6 +298,72 @@ app.post('/addProject', function(req, res){
 });
 
 /*
+* Save the project sharing settings and redirect to management.jade
+*/
+app.post('/save_project/:projectname', function(req, res) {
+	
+});
+
+/*
+* Delete the project. If it's global, remove it from user's array, otherwise also from DB.
+* Return the user to the management.jade page
+*/
+app.get('/delete_project/:projectname', function(req, res){
+	var projectName = req.params.projectname;
+	if(req.session.userAuthenticated){
+		//user connected, check if he/she has the project
+		mongo.db('localhost:8888/genesis_db').collection('users').find({"Username" : req.session.username}).toArray(function (err, user){
+			var projs = user[0].Projects;
+			for(var i = 0; i < projs.length; i++){
+				if(projs[i].name == req.params.projectname){
+					console.log("found the project with this name");
+					//found! check if it's private or public
+					if(projs[i].open == 0){
+						console.log("project closed");
+						//private, delete everything
+						//first from the db itself
+						var revisions = projs[i].revisions;
+						for(var j = 0; j < revisions.length; j++){
+							mongo.db('localhost:8888/genesis_db').collection(projectName + '_rev' + revisions[j] + '_nodes').drop();
+							mongo.db('localhost:8888/genesis_db').collection(projectName + '_rev' + revisions[j] + '_edges').drop();
+						}
+						
+						//from the projects array
+						mongo.db('localhost:8888/genesis_db').collection('projects').remove({"name" : projectName});
+						
+						//then from the list of the user
+						mongo.db('localhost:8888/genesis_db').collection('users').update({"Username" : req.session.username}, {$pull : {"Projects" : {"name" : projectName}}}, function(){
+							var usr = {
+								name: req.session.username,
+								projects: req.session.projects,
+							};
+							//render the management with user connected
+							res.render(__dirname + '/pages/management.jade', {
+								userInfo: usr,
+							});
+						});
+					}
+					else {
+						//public, remove it ONLY from the list of the user
+						console.log("project open");
+						mongo.db('localhost:8888/genesis_db').collection('users').update({"Username" : req.session.username}, {$pull : {"Projects" : {"name" : projectName}}}, function(){
+							var usr = {
+								name: req.session.username,
+								projects: req.session.projects,
+							};
+							//render the management with user connected
+							res.render(__dirname + '/pages/management.jade', {
+								userInfo: usr,
+							});
+						});
+					}
+				}
+			}
+		});
+	}
+});
+
+/*
 * Redirect for showing the project
 */
 app.get('/show_project/:projectname', function(req, res){
@@ -280,8 +377,16 @@ app.get('/show_project/:projectname', function(req, res){
 			//newJson.name = nodes.project;
 			newJson.children = [];
 			
+			var usr = {
+				name: req.session.username,
+				projects: req.session.projects
+			};
+			
+			console.log("PROJECT IN SESSION VAR : " + req.session.projects);
+			
 			//nodes: and edges: will be dropped soon
 			res.render(__dirname + '/pages/viz.jade', {
+				userInfo: usr,
 				vizJson: newJson,
 				nodes: {},
 				edges: {},
@@ -300,15 +405,224 @@ app.get('/get_data/:projectname/:rev', function(req, res){
 			//if (req.xhr) {
 		    // respond with the each user in the collection
 		    // passed to the "user" view
-			 	console.log(JSON.stringify(nodes[0].nodes));
+			if(nodes[0]){
 				res.send({
 					nodes: nodes[0].nodes,
 					edges: edges[0].edges,
 				});
+			}
+			else {
+				res.send({
+					nodes: "ERROR",
+					edges: "ERROR",
+				});
+			}	
 			//}
 		});
 	});
 });
+
+/*
+* Route for the AJAX requests for the histories
+*/
+app.get('/get_history/:projectname', function(req, res){
+	//history to pass at the beginning
+	target = {}
+	
+	var nmn_connect = require('mongoose/node_modules/mongodb').connect;
+
+	nmn_connect('mongo://localhost:8888/genesis_db', function(err, db) {
+		var interestingCollections = new Array();
+		var numberOfColl = 0;
+		var counter = 0;
+		var biggest = 0;
+		if(!err){
+			db.collectionNames(function(err, names){
+				if(!err){
+					for(var i = 0; i < names.length; i++){
+						var collName = names[i].name.slice(names[i].name.indexOf('.') + 1);
+						var projNameFromCollName = collName.slice(0, collName.indexOf('_'));
+						var typeOfColl = collName.slice(collName.indexOf('_') + 1, collName.lastIndexOf('_'));
+						//check if the collection is for our project
+						if(projNameFromCollName == req.params.projectname && typeOfColl == "history"){
+							var revNum = names[i].name.slice(names[i].name.lastIndexOf('_') + 1);
+							if(biggest < parseInt(revNum))
+								biggest = parseInt(revNum);
+							//the collection on which I want to iterate!! :) and it's from the history!
+							interestingCollections.push(collName);
+						}
+					}
+					
+					numberOfColl = interestingCollections.length;
+					for(var i = 0 ; i < interestingCollections.length; i++){
+						//console.log(interestingCollections[i]);
+						mongo.db('localhost:8888/genesis_db').collection(interestingCollections[i]).find({}, {modifiedFiles : 0, addedFiles: 0, deletedFiles: 0}).toArray(function(err, hist){
+							
+							//get that history, give it to the cleaner, save it
+							//back in the target, continue with the next
+							target = cleaner.cleanHistory(hist[0], target);
+							counter = counter + 1;
+							target.last = biggest;
+							if(counter == numberOfColl){
+								//console.log(JSON.stringify(target));
+								res.send({
+									history : target,
+								});
+							}
+						});
+					}
+				db.close();
+				}
+			});     
+		}else{
+			console.log(err)
+		}
+	});
+});
+
+/*
+* Route for the AJAX request for the deleted files 
+* in one history for a specified project
+*/
+app.get('/get_data/:projectname/history/deleted_files/:rev', function(req, res){
+	var searchedRevision = req.params.rev;
+	var smallestFound = 9007199254740992; //max integer in javascript
+	var nmn_connect = require('mongoose/node_modules/mongodb').connect;
+	
+	nmn_connect('mongo://localhost:8888/genesis_db', function(err, db) {
+		if(!err){
+			db.collectionNames(function(err, names){ // what I was looking for
+				if(!err){
+					for(var i = 0; i < names.length; i++){
+						var collName = names[i].name.slice(names[i].name.indexOf('.') + 1, names[i].name.lastIndexOf('_'));
+						var projNameFromCollName = collName.slice(0, collName.indexOf('_'));
+						//check if the collection is for our project
+						if(projNameFromCollName == req.params.projectname){
+							var revNum = parseInt(names[i].name.slice(names[i].name.lastIndexOf('_') + 1));
+							if(revNum >= searchedRevision && revNum < smallestFound){
+								smallestFound = revNum;
+							}
+						}
+						
+					}
+					
+					//create the name of the collection searched
+					var collectionName = req.params.projectname + "_history_" + smallestFound;
+					mongo.db('localhost:8888/genesis_db').collection(collectionName).find().toArray(function(err, hist){
+						var stringRev = "" + searchedRevision;
+						console.log(hist[0][stringRev].deletedFiles);
+						if(hist[0][stringRev].deletedFiles != undefined)
+							var returnValue = cleaner.cleanFileOp(hist[0][stringRev].deletedFiles[0]);
+						else
+							var returnValue = 0;
+						res.send({
+							deletedFiles : returnValue,
+						});
+					});
+				db.close();
+				}
+			});     
+		}else{
+			console.log(err)
+		}
+	});
+});
+
+/*
+* Route for the AJAX request for the added files 
+* in one history for a specified project
+*/
+app.get('/get_data/:projectname/history/added_files/:rev', function(req, res){
+	var searchedRevision = req.params.rev;
+	var smallestFound = 9007199254740992; //max integer in javascript
+	var nmn_connect = require('mongoose/node_modules/mongodb').connect;
+	
+	nmn_connect('mongo://localhost:8888/genesis_db', function(err, db) {
+		if(!err){
+			db.collectionNames(function(err, names){ // what I was looking for
+				if(!err){
+					for(var i = 0; i < names.length; i++){
+						var collName = names[i].name.slice(names[i].name.indexOf('.') + 1, names[i].name.lastIndexOf('_'));
+						var projNameFromCollName = collName.slice(0, collName.indexOf('_'));
+						//check if the collection is for our project
+						if(projNameFromCollName == req.params.projectname){
+							var revNum = parseInt(names[i].name.slice(names[i].name.lastIndexOf('_') + 1));
+							if(revNum >= searchedRevision && revNum < smallestFound){
+								smallestFound = revNum;
+							}
+						}
+					}
+					
+					//create the name of the collection searched
+					var collectionName = req.params.projectname + "_history_" + smallestFound;
+					mongo.db('localhost:8888/genesis_db').collection(collectionName).find().toArray(function(err, hist){
+						var stringRev = "" + searchedRevision;
+						console.log(hist[0][stringRev].addedFilesCount);
+						if(hist[0][stringRev].addedFiles[0] != undefined)
+							var returnValue = cleaner.cleanFileOp(hist[0][stringRev].addedFiles[0]);
+						else
+							var returnValue = 0;
+						res.send({
+							addedFiles : returnValue,
+						});
+					});
+				db.close();
+				}
+			});     
+		}else{
+			console.log(err)
+		}
+	});
+});
+
+/*
+* Route for the AJAX request for the modified files 
+* in one history for a specified project
+*/
+app.get('/get_data/:projectname/history/modified_files/:rev', function(req, res){
+	var searchedRevision = req.params.rev;
+	var smallestFound = 9007199254740992; //max integer in javascript
+	var nmn_connect = require('mongoose/node_modules/mongodb').connect;
+	
+	nmn_connect('mongo://localhost:8888/genesis_db', function(err, db) {
+		if(!err){
+			db.collectionNames(function(err, names){ // what I was looking for
+				if(!err){
+					for(var i = 0; i < names.length; i++){
+						var collName = names[i].name.slice(names[i].name.indexOf('.') + 1, names[i].name.lastIndexOf('_'));
+						var projNameFromCollName = collName.slice(0, collName.indexOf('_'));
+						//check if the collection is for our project
+						if(projNameFromCollName == req.params.projectname){
+							var revNum = parseInt(names[i].name.slice(names[i].name.lastIndexOf('_') + 1));
+							if(revNum >= searchedRevision && revNum < smallestFound){
+								smallestFound = revNum;
+							}
+						}
+					}
+					
+					//create the name of the collection searched
+					var collectionName = req.params.projectname + "_history_" + smallestFound;
+					mongo.db('localhost:8888/genesis_db').collection(collectionName).find().toArray(function(err, hist){
+						var stringRev = "" + searchedRevision;
+						console.log(hist[0][stringRev].modifiedFiles);
+						if(hist[0][stringRev].modifiedFiles[0] != undefined)
+							var returnValue = cleaner.cleanFileOp(hist[0][stringRev].modifiedFiles[0]);
+						else
+							var returnValue = 0;
+						res.send({
+							modifiedFiles : returnValue,
+						});
+					});
+				db.close();
+				}
+			});     
+		}else{
+			console.log(err)
+		}
+	});
+});
+
+
 
 function getRelation(edges, relationName) {
 	return edges[0].edges[relationName];
@@ -392,7 +706,7 @@ var getNodeFromId = function(nodes, uniqueid){
 
 
 app.get('/test', function(req, res){
-	ProjectSchema.find({ProjectName: "ArgoUML", Revision: 300}, function(err, results){
+	/*ProjectSchema.find({ProjectName: "ArgoUML", Revision: 300}, function(err, results){
 	}); 
 	
 	ProjectSchema.where('ProjectName', 'ArgoUML').run(function(err, results){
@@ -401,6 +715,9 @@ app.get('/test', function(req, res){
 	var a = mongoose.connection.db.collection("ArgoUML_rev300_edges", function (err, collection) {
 	    collection.find().toArray(function(a,b) { 
 		});
+	});*/
+	mongo.db('localhost:8888/genesis_db').collection().ToArray(function (err, colls){
+		console.log(JSON.stringify("COLLS : " + colls));
 	});
 });
 
