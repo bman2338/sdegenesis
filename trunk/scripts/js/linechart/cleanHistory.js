8ccddf508a0dc8f004fd677ae54d79a5b711d656
luca.ponzanelli@gmail.com <luca.ponzanelli@gmail.com@142c74db-416a-1121-bd68-d796f0770063>
2011-12-16 12:43:48 +0000

    linechart...

---
+++ trunk/scripts/js/linechart/cleanHistory.js
@@ -0,0 +1,97 @@
+	function cleanHistory(hist, target) {
+		
+		var newHist = {};
+		if(target) {
+			newHist = target;
+		}
+	
+		for(var e in hist) {
+			if("_id" == e)  {
+				continue;
+			}
+			var entry = hist[e];
+			entry.author = entry.author[0];
+			entry.date = entry.date[0];
+			entry.addedFilesCount = entry.addedFilesCount[0];
+			entry.deletedFilesCount = entry.deletedFilesCount[0];
+			entry.modifiedFilesCount = 	entry.modifiedFilesCount[0];
+			entry.project = entry.project[0];
+			entry.hasMse = entry.hasMse[0];
+
+            newHist[e] = cleanFileOps(entry);
+		}
+        
+        
+		return newHist;
+	};
+
+    function cleanFileOps(entry) {
+         var iter = [ "deletedFiles", "addedFiles", "modifiedFiles" ];
+			for(var it in iter) {
+				var fileOp = entry[iter[it]];
+                if(fileOp)
+                    cleanFileOp(fileOp);
+			}    
+        return entry;
+    }
+    
+    function cleanFileOp(fileOp) {
+        for (var le in fileOp) {
+			fileOp[le] = fileOp[le][0];
+		}
+		return fileOp;
+    }
+
+	function historyToD3Format(hist) {
+		var data = {};
+		var maxYear = 0;
+		var minYear = -1;
+		var dates = [];
+		var format = d3.time.format(getHistoryDateFormatString())
+		for(var e in hist) {
+			var entry = hist[e];
+			if (entry.date == undefined)
+				continue;
+			entry.number = e;
+			var dateStr = getDateStrNoZone(entry.date);
+			
+			var fd = format.parse(dateStr);
+			var year = fd.getFullYear();
+            maxYear = Math.max(year,maxYear);
+			if (minYear == -1)
+				minYear = year;
+			else
+				minYear = Math.min(year,minYear);
+            fd.setMinutes(0);
+            fd.setHours(0);
+            fd.setSeconds(0);
+            
+			var date = format(fd);
+			if(!data[date]) {
+				data[date] = [];
+				dates.push(date);
+			}
+			//TODO make copy
+			data[date].push(entry);
+			
+		}
+		return { "dates": dates, "data" : data, "minYear": minYear, "maxYear": maxYear };
+	}
+	
+	function getDateStrNoZone(dateStr) {
+		var arr = dateStr.split(" ");
+		var res = "";
+		arr[4] = " ";
+
+		return res.concat(arr);
+	}
+	
+
+	function getHistoryDateFormatString() {
+		return "%a,%b,%d,%X, ,%Y";
+	}
+	
+	function getD3DataFormatString() {
+		return "%Y-%m-%d";
+	}
+	
